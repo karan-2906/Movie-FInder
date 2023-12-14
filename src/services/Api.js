7ef830be9f1d4d4ea41845966a8505edd1c42973
karan-2906 <karangandhi486@gmail.com>
2023-12-14 19:42:08 +0530

    search

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,6 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const page = 1;
 
+const API_KEY = import.meta.env.VITE_APIKEY
+// console.log(API_KEY);
+
 export const tmdbAPI = createApi({
   reducerPath: "tmdbAPI",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3/" }),
@@ -10,7 +13,7 @@ export const tmdbAPI = createApi({
 
     // Get Genres
     getGenres: builder.query({
-      query: () => `genre/movie/list?api_key=c643a6685dfefa1bd8e9df8ab94f6004`,
+      query: () => `genre/movie/list?api_key=${API_KEY}`,
     }),
     // Get movies by type
     getMovies: builder.query({
@@ -18,21 +21,21 @@ export const tmdbAPI = createApi({
 
         // Search movies
         if(searchQuery){
-          return `/search/movie?query=${searchQuery}&page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${API_KEY}`
       }
 
         // Get movies by Genre
         if (genreName && typeof genreName === 'number') {
-          return `discover/movie?with_genres=${genreName}&page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`
+          return `discover/movie?with_genres=${genreName}&page=${page}&api_key=${API_KEY}`
         }
         // Get Popular Movies
-        return `movie/popular?page=${page}&api_key=c643a6685dfefa1bd8e9df8ab94f6004`;
+        return `movie/popular?page=${page}&api_key=${API_KEY}`;
       }
     }),
     // Get Movie
     getMovie: builder.query({
       query: (id) =>
-        `/movie/${id}?append_to_response=videos,credits&api_key=c643a6685dfefa1bd8e9df8ab94f6004`,
+        `/movie/${id}?append_to_response=videos,credits&api_key=${API_KEY}`,
     }),
   }),
 });
